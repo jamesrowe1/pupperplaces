32bbc2b734dc541518601842036d034b093b2aef
James Rowe <jamesrowe@outlook.com>
2020-07-23 18:49:07 -0400

    added search

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-function getMapPlaces() {
+var x = document.getElementById("demo");
+var lat;
+var lon;
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  lat = position.coords.latitude;
+  lon = position.coords.longitude;
+  console.log(lat);
+  console.log(lon);
+  getMapPlaces(lat, lon);
+}
+
+function getMapPlaces(lat, lon) {
   var key =
     "_961jkjpPEKGMX6YlEZm8awCLH1avefv5RUIhm6ciV_8kfRRr-gRay5GIICt9Ih-ggqoKNJdnSD7rBuIwmcbiaHSLUFWeJmOaHmzO5t4UwYvJCfX7hy38gy4IhUWX3Yx";
   var settings = {
@@ -14,7 +33,11 @@ function getMapPlaces() {
       //"content-type": "application/x-www-form-urlencoded",
     },
     data: {
-      location: "Harrisburg",
+      latitude: lat,
+      longitude: lon,
+      radius: 2000,
+      //sort_by: "distance",
+      //location: "Fort+Lee",
       term: "dog+friendly",
       accessToken:
         "_961jkjpPEKGMX6YlEZm8awCLH1avefv5RUIhm6ciV_8kfRRr-gRay5GIICt9Ih-ggqoKNJdnSD7rBuIwmcbiaHSLUFWeJmOaHmzO5t4UwYvJCfX7hy38gy4IhUWX3Yx",
@@ -23,10 +46,29 @@ function getMapPlaces() {
 
   $.ajax(settings).done(function (response) {
     console.log(response);
-    getSpecificBusiness("V1ieDWzBTCa6mrvjlvn8iQ");
+    printToPage(response);
+    //getSpecificBusiness("V1ieDWzBTCa6mrvjlvn8iQ");
   });
 }
+function printToPage(response) {
+  businessArr = response.businesses;
 
+  businessArr.forEach((business) => {
+    var businessDiv = $("<div>");
+    var nameDiv = $("<div>");
+    var phoneDiv = $("<div>");
+    var addressDiv = $("<div>");
+
+    nameDiv.append(business.name);
+
+    phoneDiv.append(business.display_phone);
+    addressDiv.append(business.location.display_address);
+    businessDiv.append(nameDiv);
+    businessDiv.append(phoneDiv);
+    businessDiv.append(addressDiv);
+    $("#everything").append(businessDiv);
+  });
+}
 function getSpecificBusiness(id) {
   var key =
     "_961jkjpPEKGMX6YlEZm8awCLH1avefv5RUIhm6ciV_8kfRRr-gRay5GIICt9Ih-ggqoKNJdnSD7rBuIwmcbiaHSLUFWeJmOaHmzO5t4UwYvJCfX7hy38gy4IhUWX3Yx";
@@ -54,4 +96,4 @@ function getSpecificBusiness(id) {
   });
 }
 
-getMapPlaces();
+getLocation();
